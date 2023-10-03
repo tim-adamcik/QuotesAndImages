f712b9a6e88fa1fb00a986f7bb0a88b430081357
Timothy Adamcik <tadamcik0425@gmail.com>
2023-10-03 17:51:01 -0400

    add webservice with get random images and get random image async throw functions

--- QuotesAndImages/Services/WebService.swift
+++ QuotesAndImages/Services/WebService.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case badUrl
+    case invalidImageId(Int)
+    case decodingError
+}
+
+class WebService {
+    func getRandomImages(ids: [Int]) async throws -> [RandomImage] {
+        var randomImages: [RandomImage] = []
+        for id in ids {
+            let randomImage = try await getRandomImage(id: id)
+            randomImages.append(randomImage)
+        }
+        return randomImages
+    }
+    
+    private func getRandomImage(id: Int) async throws -> RandomImage {
+        guard let url = Constants.Urls.getRandomImageUrl() else {
+            throw NetworkError.badUrl
+        }
+        
+        guard let randomQuoteUrl = Constants.Urls.randomQuoteUrl else {
+            throw NetworkError.badUrl
+        }
+        
+        async let (imageData,_) = URLSession.shared.data(from: url)
+        async let (randomQuoteData,_) = URLSession.shared.data(from: randomQuoteUrl)
+        
+        guard let quote = try? JSONDecoder().decode(Quote.self, from: try await randomQuoteData) else {
+            throw NetworkError.decodingError
+        }
+        
+        return RandomImage(image: try await imageData, quote: quote)
+    }
+    
+    
+}
