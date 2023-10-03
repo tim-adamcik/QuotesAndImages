8b13924e1b7b3c8306ae47eccc76eb63bd1e26ff
Timothy Adamcik <tadamcik0425@gmail.com>
2023-10-03 17:23:42 -0400

    Initial Commit

---
+++ QuotesAndImages/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuotesAndImages
+//
+//  Created by Timothy Adamcik on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuotesAndImages/QuotesAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAndImagesApp.swift
+//  QuotesAndImages
+//
+//  Created by Timothy Adamcik on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
